f5397f18779437dbd9012ef4d761205681fc0bbc
TeamAbella060420 <jabella@rscm.com.sa>
2022-04-02 11:59:25 +0300

    Update random.js

--- js/random.js
+++ js/random.js
@@ -1,6 +1,9 @@
-function getRandomNumber () {
- const randomNumber = Math.floor( Math.random() * 6 ) + 1;
- alertrandomNumber;
+function getRandomNumber (upper) {
+ const randomNumber = Math.floor( Math.random() * upper ) + 1;
+ return randomNumber;
 }
 
-const dieRoll = getRandomNumber ();
+console.log( getRandomNumber(6));
+console.log( getRandomNumber(100));
+console.log( getRandomNumber(1000));
+console.log( getRandomNumber(20));
