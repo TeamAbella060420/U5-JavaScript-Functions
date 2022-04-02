5293e4a4f7b577d66ae2870dcf808f3681e0f3b8
TeamAbella060420 <jabella@rscm.com.sa>
2022-04-02 11:59:30 +0300

    Update scope.js

--- js/scope.js
+++ js/scope.js
@@ -1,4 +1,17 @@
+let person = 'Lee';
+
 function greeting() {
+  // Function Scope
   let person = 'Meg';
   alert(`Hi, ${person}!`);
 }
+
+function greeting2() {
+  // Function Scope
+  let person = 'Robert';
+  alert(`Goodmorning, ${person}!`);
+}
+
+greeting();
+alert(`Hi, ${person}!`)
+greeting2();
